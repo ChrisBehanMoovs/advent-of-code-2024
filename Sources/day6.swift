0c34b7bb5f8daa5564885b77ea3389030560d80b
CharkMuckerberg <chris@moovsapp.com>
2024-12-24 16:03:59 -0500

    Day 6 part 2

--- Sources/day6.swift
+++ Sources/day6.swift
@@ -1,8 +1,9 @@
 import Foundation
 
-struct position {
+struct position: Hashable {
     var x: Int
     var y: Int
+    var dir: Character
 }
 
 let velocity: [Character: (Int, Int)] = [
@@ -20,8 +21,10 @@ let rotate: [Character: Character] = [
 ]
 
 func day6() {
+    print("Day 6")
     let map = getMap(filepath: "Sources/inputs/day6/input.txt")
     part1(map: map)
+    part2(map: map)
 }
 
 func getMap(filepath: String) -> [[Character]] {
@@ -30,23 +33,23 @@ func getMap(filepath: String) -> [[Character]] {
     return map
 }
 
-func findGuard(map: [[Character]]) -> (position, Character) {
+func findGuard(map: [[Character]]) -> position {
     for y in 0..<map.count {
         for x in 0..<map[0].count {
             if ["^", ">", "V", "<"].contains(map[y][x]) {
-                return (position(x: x, y: y), map[y][x])
+                return position(x: x, y: y, dir: map[y][x])
             }
         }
     }
-    return (position(x: -1, y: -1), " ")
+    return position(x: -1, y: -1, dir: " ")
 }
 
 func part1(map: [[Character]]) {
     // var MapForTracking = map
     var MapForMoving = map
-    var (pos, dir) = findGuard(map: MapForMoving)
+    var pos = findGuard(map: MapForMoving)
     while true {
-        (pos, dir) = moveGuard(map: &MapForMoving, pos: pos, dir: dir)
+        pos = moveGuard(map: &MapForMoving, pos: pos)
         if pos.x == -1 {
             break
         }
@@ -70,21 +73,52 @@ func isInBounds(pos: position, map: [[Character]]) -> Bool {
 }
 
 func moveGuard(
-    map: inout [[Character]], pos: position, dir: Character
-) -> (position, Character) {
-    let (yChange, xChange) = velocity[dir]!
-    let nextPos = position(x: pos.x + xChange, y: pos.y + yChange)
+    map: inout [[Character]], pos: position
+) -> (position) {
+    let (yChange, xChange) = velocity[pos.dir]!
+    let nextPos = position(x: pos.x + xChange, y: pos.y + yChange, dir: pos.dir)
 
     if !isInBounds(pos: nextPos, map: map) {
         markVisited(map: &map, pos: pos)
-        return (position(x: -1, y: -1), " ")
+        return position(x: -1, y: -1, dir: " ")
     }
 
-    if canMoveOnSpace(map: &map, pos: nextPos) {  
+    if canMoveOnSpace(map: &map, pos: nextPos) {
         markVisited(map: &map, pos: pos)
-        return (nextPos, dir)
-    } else {  
-        let newDir = rotate[dir]!
-        return (pos, newDir)
+        return nextPos
+    } else {
+        let rotatedPos = position(x: pos.x, y: pos.y, dir: rotate[pos.dir]!)
+        return rotatedPos
     }
+}
+
+func isStuckInLoop(map: inout [[Character]], startingPos: position) -> Bool {
+    var visited: Set<(position)> = []
+    var pos = findGuard(map: map)
+    while !visited.contains(pos) {
+        visited.insert(pos)
+        pos = moveGuard(map: &map, pos: pos)
+        if pos.x == -1 {  // left map
+            return false
+        }
+    }
+    return true
+}
+
+func part2(map: [[Character]]) {
+    var loopCount = 0
+    let startingPos = findGuard(map: map)
+    for y in 0..<map.count {
+        for x in 0..<map[0].count {
+            var newMap = map
+            guard newMap[y][x] == "." else {
+                continue
+            }
+            newMap[y][x] = "#"
+            if isStuckInLoop(map: &newMap, startingPos: startingPos) {
+                loopCount += 1
+            }
+        }
+    }
+    print("Part 2 answer: \(loopCount)")
 }
